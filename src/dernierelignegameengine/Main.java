69c198f5ea15dcc6bc4ad62af911468dd62a5543
gaussreload <gaussreload@sylvanas>
2014-10-22 14:42:09 +0200

    feat(TurnPlayer, Main): Turn by turn implementation.
    Creating a turn by turn game mechanism allowing only the player whose turn it is to play. For a player, playing while then give the hand to the next non null player (for less than 8 players). If a player is the only player in game, a message is displayed but he can still play alone. The first player to play is arbitrarily chosen.

---
+++ src/dernierelignegameengine/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dernierelignegameengine;
+
+/**
+ *
+ * @author gaussreload
+ */
+public class Main {
+    public static void main(String[] Args) {
+        TurnPlayer[] players = new TurnPlayer[8];
+        //for (int i=0; i <8; i++)
+            //players[i] = new TurnPlayer("Joueur " + i, i);
+        
+        players[0] = new TurnPlayer("Joueur " + 0, 0);
+        
+        players[0].play(players);
+        
+    }
+}
