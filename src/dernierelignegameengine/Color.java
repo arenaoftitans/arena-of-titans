7a212fd6dab0ea9c2664060b854045c5b49b0821
gaussreload <gaussreload@hellscream.home>
2014-10-28 13:35:07 +0100

    doc(Color.java): Commenting using the Javadoc convention.

--- src/dernierelignegameengine/Color.java
+++ src/dernierelignegameengine/Color.java
@@ -1,9 +1,15 @@
 package dernierelignegameengine;
 
+/**
+ * <b>Enumeration for the colors available for the cards and the board.</b>
+ * 
+ * @author "Dernière Ligne" first development team
+ * @version 1.0
+ */
 public enum Color {
     BLACK,
     BLUE,
     RED,
     YELLOW,
-    WHITE,
+    WHITE;
 }
