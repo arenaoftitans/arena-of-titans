4413c33d8c0d694e3eab0b0bb31aca2ad67c92a9
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-11-12 16:28:56 +0100

    doc(SvgBoardGeneratorTest): remove empty doc and license header.

--- src/test/java/com/derniereligne/engine/board/SvgBoardGeneratorTest.java
+++ src/test/java/com/derniereligne/engine/board/SvgBoardGeneratorTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.derniereligne.engine.board;
 
 import com.derniereligne.engine.Color;
@@ -27,10 +23,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
 
-/**
- *
- * @author jenselme
- */
 public class SvgBoardGeneratorTest {
 
     private SvgBoardGenerator svgBoardGenerator;
