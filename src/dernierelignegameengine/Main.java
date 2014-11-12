3a16e56e2e840ae2e881640bb5a16433efc77968
Robin Waginaire <robin.waginaire@centrale-marseille.fr>
2014-11-12 13:47:11 +0100

    ref(main): Improvement in game for one player
    I created several methods to regroup my code and I translated variables with French names in English.

--- src/dernierelignegameengine/Main.java
+++ src/dernierelignegameengine/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] Args) {
-        Player[] players = new Player[8];
+        /*Player[] players = new Player[8];
         for (int i=0; i <8; i++)
             players[i] = new Player("Joueur " + i, null, null, i);
 
@@ -20,80 +20,40 @@ public class Main {
         players[0].playTurn(players);
         players[0].playTurn(players);
         players[2].playTurn(players);
-        players[1].playTurn(players);
+        players[1].playTurn(players);*/
+        gameForOnePlayer();
 
-        //partie à 1 joueur
-
-        Board plateau = new Board("standard_board");
-        Square currentSquare = plateau.getSquare(1, 8);
+    }
+    public static void gameForOnePlayer(){
+        Board board = new Board("standard_board");
+        Square currentSquare = board.getSquare(0, 8);
         currentSquare.setAsOccupied();
         boolean isOver=false;
         boolean isPossible;
         boolean isEmpty;
         Card chosenCard;
-        char carte;
-        char couleur;
+        char card;
+        char color;
         int x;
         int y=9;
 
-	Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
             while (isOver==false){
                 isPossible=false;
                 isEmpty=true;
                 chosenCard=null;
                 while(isEmpty){
-                    carte=' ';
-                    couleur=' ';
-                    while(carte != 'K' && carte != 'Q' && carte != 'A' && carte != 'B' && carte != 'W' && carte != 'M' && carte != 'R'){
-                        System.out.println("Quelle carte voulez-vous jouer? (roi: K, reine: Q, assassin: A, fou: B, guerrier: W, magicien: M, cavalier: R)");
-                        carte = sc.nextLine().charAt(0);
+                    card=' ';
+                    color=' ';
+                    while(wrongTypeCard(card)){
+                        System.out.println("Quelle carte voulez-vous jouer?\nroi: K\nreine: Q\nassassin: A\nfou: B\nguerrier: W\nmagicien: M\ncavalier: R");
+                        card = sc.nextLine().charAt(0);
                     }
-                    while(couleur != 'R' && couleur != 'B' && couleur != 'N' && couleur != 'J'){
-                        System.out.println("Quelle couleur choisissez-vous pour votre carte? (rouge: R, bleu: B, noir: N, jaune: J)");
-                        couleur = sc.nextLine().charAt(0);
+                    while(wrongTypeColor(color)){
+                        System.out.println("Quelle couleur choisissez-vous pour votre carte?\nrouge: R\nbleu: B\nnoir: N\njaune: J");
+                        color = sc.nextLine().charAt(0);
                     }
-                    if(carte=='K'){
-                        if(couleur=='N'){chosenCard = new KingCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new KingCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new KingCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new KingCard(plateau,Color.YELLOW);}
-                    }
-                    if(carte=='Q'){
-                        if(couleur=='N'){chosenCard = new QueenCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new QueenCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new QueenCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new QueenCard(plateau,Color.YELLOW);}
-                    }
-                    if(carte=='A'){
-                        if(couleur=='N'){chosenCard = new AssassinCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new AssassinCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new AssassinCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new AssassinCard(plateau,Color.YELLOW);}
-                    }
-                    if(carte=='B'){
-                        if(couleur=='N'){chosenCard = new BishopCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new BishopCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new BishopCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new BishopCard(plateau,Color.YELLOW);}
-                    }
-                    if(carte=='W'){
-                        if(couleur=='N'){chosenCard = new WarriorCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new WarriorCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new WarriorCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new WarriorCard(plateau,Color.YELLOW);}
-                    }
-                    if(carte=='M'){
-                        if(couleur=='N'){chosenCard = new WizardCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new WizardCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new WizardCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new WizardCard(plateau,Color.YELLOW);}
-                    }
-                    if(carte=='R'){
-                        if(couleur=='N'){chosenCard = new RiderCard(plateau,Color.BLACK);}
-                        if(couleur=='R'){chosenCard = new RiderCard(plateau,Color.RED);}
-                        if(couleur=='B'){chosenCard = new RiderCard(plateau,Color.BLUE);}
-                        if(couleur=='J'){chosenCard = new RiderCard(plateau,Color.YELLOW);}
-                    }
+                    chosenCard=getChosenCard(card, color, board);
                     if(chosenCard.getPossibleMovements(currentSquare).isEmpty()){
                         System.out.println("Pas de mouvements possibles. Rechoisissez.");
                     }
@@ -116,10 +76,10 @@ public class Main {
                         y=sc.nextInt();
                     }
                     sc.nextLine();
-                    if (chosenCard.getPossibleMovements(currentSquare).contains(plateau.getSquare(x, y))){
+                    if (chosenCard.getPossibleMovements(currentSquare).contains(board.getSquare(x, y))){
                         isPossible=true;
                         currentSquare.empty();
-                        currentSquare=plateau.getSquare(x, y);
+                        currentSquare=board.getSquare(x, y);
                         currentSquare.setAsOccupied();
                     }
                 }
@@ -132,11 +92,101 @@ public class Main {
                 }
             }
             System.out.println("Partie finie !");
-
-
-
-
-
-
+        };
+    public static boolean wrongTypeCard(char card){
+        return card != 'K' && card != 'Q' && card != 'A' && card != 'B' && card != 'W' && card != 'M' && card != 'R';
     }
+
+    public static boolean wrongTypeColor(char color){
+        return color != 'R' && color != 'B' && color != 'N' && color != 'J';
+    }
+
+    public static Card getChosenCard(char card, char color, Board board){
+        if(card=='K'){
+            return getColoredKingCard(color, board);
+        }
+        else if(card=='Q'){
+            return getColoredQueenCard(color, board);
+        }
+        else if(card=='A'){
+            return getColoredAssassinCard(color, board);
+        }
+        else if(card=='B'){
+            return getColoredBishopCard(color, board);
+        }
+        else if(card=='W'){
+            return getColoredWarriorCard(color, board);
+        }
+        else if(card=='M'){
+            return getColoredWizardCard(color, board);
+        }
+        else{
+            return getColoredRiderCard(color, board);
+        }
+    }
+
+    public static Card getColoredKingCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new KingCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new KingCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new KingCard(board,Color.BLUE);}
+        else{chosenCard = new KingCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+    public static Card getColoredQueenCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new QueenCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new QueenCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new QueenCard(board,Color.BLUE);}
+        else{chosenCard = new QueenCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+    public static Card getColoredAssassinCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new AssassinCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new AssassinCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new AssassinCard(board,Color.BLUE);}
+        else{chosenCard = new AssassinCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+    public static Card getColoredBishopCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new BishopCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new BishopCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new BishopCard(board,Color.BLUE);}
+        else{chosenCard = new BishopCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+    public static Card getColoredWarriorCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new WarriorCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new WarriorCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new WarriorCard(board,Color.BLUE);}
+        else{chosenCard = new WarriorCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+    public static Card getColoredWizardCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new WizardCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new WizardCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new WizardCard(board,Color.BLUE);}
+        else{chosenCard = new WizardCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+    public static Card getColoredRiderCard(char color, Board board){
+        Card chosenCard;
+        if(color=='N'){chosenCard = new RiderCard(board,Color.BLACK);}
+        else if(color=='R'){chosenCard = new RiderCard(board,Color.RED);}
+        else if(color=='B'){chosenCard = new RiderCard(board,Color.BLUE);}
+        else{chosenCard = new RiderCard(board,Color.YELLOW);}
+        return chosenCard;
+    }
+
+
 }
