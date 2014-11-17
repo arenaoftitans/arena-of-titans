b1a036568f89b36f55b145e214fc459f399def53
Maxime Choulet <maxime.choulet@centrale-marseille.fr>
2014-11-17 17:03:02 +0100

    fix(GameFactory.java) : removing useless imports

--- src/main/java/com/derniereligne/engine/GameFactory.java
+++ src/main/java/com/derniereligne/engine/GameFactory.java
@@ -15,8 +15,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
