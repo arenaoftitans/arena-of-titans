a3a12c5770036e6a8df7d56f9b5a6ee572806afe
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-11-24 14:05:16 +0100

    fix(game.js): indentation.

--- src/main/webapp/inc/js/game.js
+++ src/main/webapp/inc/js/game.js
@@ -7,141 +7,141 @@
  * @returns {undefined}
  */
 function showHttpError(data) {
-  if (data.hasOwnProperty('error_to_display')) {
-    alert(data.error_to_display);
-  } else {
-    console.log(data.error);
-  }
+    if (data.hasOwnProperty('error_to_display')) {
+        alert(data.error_to_display);
+    } else {
+        console.log(data.error);
+    }
 }
 
 var nameSpace = angular.module("lastLine", []);
 
 nameSpace.controller("game", ['$scope', '$http', function ($scope, $http) {
-    $scope.highlightedSquares = []; // Stores the ids of the squares that are highlighted.
-    $scope.currentCard = {};
-    $scope.currentPlayerIndex = 0;
-    $scope.squareX = '';
-    $scope.squareY = '';
+        $scope.highlightedSquares = []; // Stores the ids of the squares that are highlighted.
+        $scope.currentCard = {};
+        $scope.currentPlayerIndex = 0;
+        $scope.squareX = '';
+        $scope.squareY = '';
 
-    // Card names and colors. Generate buttons for all combination of cards and
-    // colors thanks to ng-repeat.
-    $scope.colors = ['blue', 'red', 'black', 'yellow'];
-    $scope.cards = ['warrior', 'wizard', 'rider', 'bishop', 'queen', 'king', 'assassin'];
+        // Card names and colors. Generate buttons for all combination of cards and
+        // colors thanks to ng-repeat.
+        $scope.colors = ['blue', 'red', 'black', 'yellow'];
+        $scope.cards = ['warrior', 'wizard', 'rider', 'bishop', 'queen', 'king', 'assassin'];
 
-    function resetHighlightedSquares() {
-      for (var index in $scope.highlightedSquares) {
-        var id = $scope.highlightedSquares[index];
-        angular.element(id).removeAttr('style', $scope.squaresDefaultStyle);
-      }
-    }
-
-    function highlightSquares() {
-      for (var index in $scope.highlightedSquares) {
-        var id = $scope.highlightedSquares[index];
-        angular.element(id).attr("style", "fill:green");
-      }
-    }
-
-    function movePlayerTo(playerIndex, squareId) {
-      var playerId = 'player' + playerIndex;
-      var circleToDelete = document.getElementById(playerId);
-      if (circleToDelete) {
-        circleToDelete.parentNode.removeChild(circleToDelete);
-      }
-      var square = angular.element(squareId);
-      var height = parseInt(square.attr('height'));
-      var width = parseInt(square.attr('width'));
-      var x = parseInt(square.attr('x')) + width / 2;
-      var y = parseInt(square.attr('y')) + height / 2;
-      var radius = width / 4;
-      var transform = square.attr('transform');
-      var player = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-      player.setAttribute('id', playerId);
-      player.setAttribute('cx', x);
-      player.setAttribute('cy', y);
-      player.setAttribute('r', radius);
-      player.setAttribute('transform', transform);
-      document.getElementById('boardLayer').appendChild(player);
-    }
-
-    // Function called when a button is clicked.
-    $scope.viewPossibleMovements = function (card, color) {
-
-      // Do a GET on a rest URL. Transmit the name of the card, its color and
-      // the current position of the player. Get a list of the ids of the
-      // squares on which the player can move.
-      $http({
-        url: '/DerniereLigneGameEngine/rest/getPossibleSquares',
-        method: "GET",
-        params: {
-          card_name: card,
-          card_color: color,
-          player_id: $scope.currentPlayerIndex
+        function resetHighlightedSquares() {
+            for (var index in $scope.highlightedSquares) {
+                var id = $scope.highlightedSquares[index];
+                angular.element(id).removeAttr('style', $scope.squaresDefaultStyle);
+            }
         }
-      })
-              .success(function (data) {
-                resetHighlightedSquares();
 
-                $scope.highlightedSquares = data;
+        function highlightSquares() {
+            for (var index in $scope.highlightedSquares) {
+                var id = $scope.highlightedSquares[index];
+                angular.element(id).attr("style", "fill:green");
+            }
+        }
 
-                highlightSquares();
+        function movePlayerTo(playerIndex, squareId) {
+            var playerId = 'player' + playerIndex;
+            var circleToDelete = document.getElementById(playerId);
+            if (circleToDelete) {
+                circleToDelete.parentNode.removeChild(circleToDelete);
+            }
+            var square = angular.element(squareId);
+            var height = parseInt(square.attr('height'));
+            var width = parseInt(square.attr('width'));
+            var x = parseInt(square.attr('x')) + width / 2;
+            var y = parseInt(square.attr('y')) + height / 2;
+            var radius = width / 4;
+            var transform = square.attr('transform');
+            var player = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+            player.setAttribute('id', playerId);
+            player.setAttribute('cx', x);
+            player.setAttribute('cy', y);
+            player.setAttribute('r', radius);
+            player.setAttribute('transform', transform);
+            document.getElementById('boardLayer').appendChild(player);
+        }
 
-                // Stores the selected card.
-                $scope.currentCard = {card_name: card, card_color: color};
-              })
-              .error(function (data) {
-                showHttpError(data);
-                $scope.currentCard = {};
-              });
-    };
+        // Function called when a button is clicked.
+        $scope.viewPossibleMovements = function (card, color) {
 
-    $scope.play = function () {
-      if ($scope.currentCard !== {}) {
-        $http({
-          url: '/DerniereLigneGameEngine/rest/play',
-          method: 'GET',
-          params: {
-            card_name: $scope.currentCard.card_name,
-            card_color: $scope.currentCard.card_color,
-            player_id: $scope.currentPlayerIndex,
-            x: $scope.squareX,
-            y: $scope.squareY
-          }
-        })
-                .success(function (data) {
-                  movePlayerTo($scope.currentPlayerIndex, data[0]);
-                  $scope.currentPlayerIndex = ($scope.currentPlayerIndex + 1) % $scope.players.length;
-                  resetHighlightedSquares();
+            // Do a GET on a rest URL. Transmit the name of the card, its color and
+            // the current position of the player. Get a list of the ids of the
+            // squares on which the player can move.
+            $http({
+                url: '/DerniereLigneGameEngine/rest/getPossibleSquares',
+                method: "GET",
+                params: {
+                    card_name: card,
+                    card_color: color,
+                    player_id: $scope.currentPlayerIndex
+                }
+            })
+                    .success(function (data) {
+                        resetHighlightedSquares();
+
+                        $scope.highlightedSquares = data;
+
+                        highlightSquares();
+
+                        // Stores the selected card.
+                        $scope.currentCard = {card_name: card, card_color: color};
+                    })
+                    .error(function (data) {
+                        showHttpError(data);
+                        $scope.currentCard = {};
+                    });
+        };
+
+        $scope.play = function () {
+            if ($scope.currentCard !== {}) {
+                $http({
+                    url: '/DerniereLigneGameEngine/rest/play',
+                    method: 'GET',
+                    params: {
+                        card_name: $scope.currentCard.card_name,
+                        card_color: $scope.currentCard.card_color,
+                        player_id: $scope.currentPlayerIndex,
+                        x: $scope.squareX,
+                        y: $scope.squareY
+                    }
                 })
-                .error(function (data) {
-                  showHttpError(data);
-                });
-      } else {
-        alert('Please select a card.');
-      }
-    };
+                        .success(function (data) {
+                            movePlayerTo($scope.currentPlayerIndex, data[0]);
+                            $scope.currentPlayerIndex = ($scope.currentPlayerIndex + 1) % $scope.players.length;
+                            resetHighlightedSquares();
+                        })
+                        .error(function (data) {
+                            showHttpError(data);
+                        });
+            } else {
+                alert('Please select a card.');
+            }
+        };
 
-    $scope.players = [];
-    $scope.numberMaximumOfPlayers = 8;
-    for (var i = 0; i < $scope.numberMaximumOfPlayers; i++) {
-      $scope.players.push({
-        index: i,
-        name: ''
-      });
-    }
+        $scope.players = [];
+        $scope.numberMaximumOfPlayers = 8;
+        for (var i = 0; i < $scope.numberMaximumOfPlayers; i++) {
+            $scope.players.push({
+                index: i,
+                name: ''
+            });
+        }
 
-    $scope.createGame = function () {
-      $http({
-        url: '/DerniereLigneGameEngine/rest/createGame',
-        method: 'POST',
-        data: $scope.players
-      })
-              .success(function (data) {
-                angular.element('#createGame').hide();
-                angular.element('#game').show();
-                $scope.players = data;
-                $scope.currentPlayerIndex = 0;
-              })
-              .error(showHttpError);
-    };
-  }]);
+        $scope.createGame = function () {
+            $http({
+                url: '/DerniereLigneGameEngine/rest/createGame',
+                method: 'POST',
+                data: $scope.players
+            })
+                    .success(function (data) {
+                        angular.element('#createGame').hide();
+                        angular.element('#game').show();
+                        $scope.players = data;
+                        $scope.currentPlayerIndex = 0;
+                    })
+                    .error(showHttpError);
+        };
+    }]);
