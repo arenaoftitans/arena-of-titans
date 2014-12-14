f01c473de6ebed2aa68404560a51e663084af07a
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-12-14 21:45:30 +0100

    ref(ModifyNumberOfMovesInATurnTrump): rename an attributes to match its function

--- src/main/java/com/derniereligne/engine/cards/trumps/ModifyNumberOfMovesInATurnTrump.java
+++ src/main/java/com/derniereligne/engine/cards/trumps/ModifyNumberOfMovesInATurnTrump.java
@@ -9,18 +9,18 @@ import com.derniereligne.engine.Player;
  * @version 1.0
  */
 public class ModifyNumberOfMovesInATurnTrump extends Trump{
-    private final int turnsToAdd;
+    private final int deltaOfMoves;
 
     public ModifyNumberOfMovesInATurnTrump(String name, int duration, String description,
-            int cost, int turnsToAdd) {
+            int cost, int deltaOfMoves) {
         super(name, duration, description, cost);
-        this.turnsToAdd = turnsToAdd;
+        this.deltaOfMoves = deltaOfMoves;
     }
 
     @Override
     public void affect(Player player) {
         if (player != null && duration > 0) {
-            player.addToNumberMoveToPlay(turnsToAdd);
+            player.addToNumberMoveToPlay(deltaOfMoves);
         }
     }
 }
