2e67db6d5951b386e132218366d41d309913cb6b
Maxime Choulet <maxime.choulet@centrale-marseille.fr>
2014-12-17 12:42:49 +0100

    test(trump) : adding unit testing for removing color trump card

---
+++ src/test/java/com/derniereligne/engine/cards/trumps/RemovingColorTrumpCardTest.java
@@ -0,0 +1,59 @@
+package com.derniereligne.engine.cards.trumps;
+
+import com.derniereligne.engine.Color;
+import com.derniereligne.engine.GameFactory;
+import com.derniereligne.engine.Match;
+import com.derniereligne.engine.Player;
+import com.derniereligne.engine.board.Board;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RemovingColorTrumpCardTest {
+    Match match;
+
+    @Before
+    public void setUp() {
+        GameFactory gf = new GameFactory();
+        Board board = gf.getBoard();
+
+        Player player1 = new Player("player1", 0);
+        Player player2 = new Player("player2", 1);
+        List<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+
+        match = new Match(players, gf.getBoard(), gf.getDeckCreator());
+    }
+
+    @After
+    public void tearDown() {
+        match = null;
+    }
+
+    @Test
+    public void testRemovingColor() {
+        Player activePlayer = match.getActivePlayer();
+        assertNotNull(activePlayer);
+        assertNotNull(activePlayer.getDeck());
+        assertNotNull(activePlayer.getDeck().getFirstCardInHand());
+        assertNotNull(activePlayer.getDeck().getFirstCardInHand().getSquarePossibleColors());
+        Color color = activePlayer.getDeck().getFirstCardInHand().getSquarePossibleColors().iterator().next();
+        TrumpCard trumpCard = new RemovingColorTrumpCard("Remove", 1, null, 0, color);
+        trumpCard.affect(activePlayer);
+        assertFalse(activePlayer.getDeck().getFirstCardInHand().getSquarePossibleColors().contains(color));
+    }
+
+    @Test
+    public void testRemovingAll() {
+        Player activePlayer = match.getActivePlayer();
+        TrumpCard trumpCard = new RemovingColorTrumpCard("Remove", 1, null, 0, Color.ALL);
+        trumpCard.affect(activePlayer);
+        assertEquals(0, activePlayer.getDeck().getFirstCardInHand().getSquarePossibleColors().size());
+    }
+}
