73eaa41a8520bf6695ee8307382ab0a56edc9d3c
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-01-28 10:34:08 +0100

    test(game-ctrl.js): add tests for viewPossibleMovements.

--- src/main/webapp/inc/js/app/tests/game/ctrl.spec.js
+++ src/main/webapp/inc/js/app/tests/game/ctrl.spec.js
@@ -1,7 +1,8 @@
 'use strict';
 
 describe('game', function () {
-    var scope, $httpBackend;
+    var $scope, controller, $httpBackend;
+    var square;
 
     var cardName = 'King';
     var cardColor = 'Red';
@@ -13,7 +14,20 @@ describe('game', function () {
     var playUrl = '/aot/rest/play';
     var playMethod = 'GET';
 
-    beforeEach(angular.mock.module('lastLine'));
+
+    beforeEach(angular.mock.module('lastLine.game'));
+
+    beforeEach(angular.mock.inject(function ($rootScope, $controller, $compile) {
+        $scope = $rootScope.$new();
+        controller = $controller;
+        controller('game', {$scope: $scope});
+        var squareHtml = '<div id="square-0-0"></div>';
+        square = angular.element(squareHtml);
+        square.attr('ng-class', "{highlightedSquare: highlightedSquares.indexOf('square-0-0') > -1}");
+        var compiled = $compile(square);
+        compiled($scope);
+        $scope.$digest();
+    }));
 
     beforeEach(angular.mock.inject(function ($rootScope, $controller, _$httpBackend_) {
         $httpBackend = _$httpBackend_;
@@ -37,45 +51,37 @@ describe('game', function () {
 
         $httpBackend.when(playMethod, playUrl)
                 .respond({
-                    "newSquare": "#0-8",
-                    "nextPlayer": {
-                        "name": "Toto",
-                        "id": "0"
+                    newSquare: "#0-8",
+                    nextPlayer: {
+                        name: "Toto",
+                        id: "0"
                     },
-                    "possibleCardsNextPlayer": [
+                    possibleCardsNextPlayer: [
                         {
-                            "name": "King",
-                            "color": "Red"
+                            name: "King",
+                            color: "Red"
                         }
                     ]
                 });
-
-        scope = $rootScope.$new();
-        $controller('game', {$scope: scope});
     }));
 
-    it('create game', function () {
-        scope.createGame();
-        $httpBackend.flush();
-        expect(scope.currentPlayer).toEqual({
-            name: "Toto",
-            id: "0"
-        });
+
+    it('should have scope to be defined', function () {
+        expect($scope).toBeDefined();
     });
 
-    it('view possible movements', function () {
-        scope.viewPossibleMovements(cardName, cardColor);
+    it('view possible movements: correct card selected', function () {
+        $scope.viewPossibleMovements(cardName, cardColor);
         $httpBackend.flush();
-        expect(scope.highlightedSquares).toEqual(["square-0-0", "square-1-1"]);
-        expect(scope.selectedCard).toEqual({card_name: cardName, card_color: cardColor});
+        expect($scope.selectedCard).toEqual({card_name: cardName, card_color: cardColor});
     });
 
-    it('play', function () {
-        var squareId = 'square-0-0';
-        var x = '0';
-        var y = '0';
-        scope.viewPossibleMovements(cardName, cardColor);
-        //scope.play(squareId, x, y);
+    it('view possible movements: correct squares are highlighted', function () {
+        $scope.viewPossibleMovements(cardName, cardColor);
+        $httpBackend.flush();
+        expect($scope.highlightedSquares).toEqual(["square-0-0", "square-1-1"]);
+        $scope.$digest();
+        expect(square.attr('class')).toContain('highlightedSquare');
     });
 
 });
