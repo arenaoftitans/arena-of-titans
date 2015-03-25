494e479094002760df7c1506ea3bdaa1385be878
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-03-25 10:01:24 +0100

    fix(it/spec/create-game): Start fixing protractor tests.
    - The last test (true game creation) doesn't work yet. Troubles with
    running two browser on the same test.

--- src/test/webapp/it/spec/create-game.spec.js
+++ src/test/webapp/it/spec/create-game.spec.js
@@ -1,10 +1,10 @@
 /* global by, element, browser, expect, $$ */
 
 describe('create game', function () {
-    var player1Input, player2Input;
-    var createGameUrl = '/createGame';
+    var createGameUrl = '/game';
     var notEnoughPlayersMessage = 'Not enough players. 2 Players at least are required to start a game';
     var numberPlayers = 8;
+    var initialNumberOfPlayers = 2;
     var createGameForm = element(by.buttonText('Create game'));
 
     function submitFormNotEngouthPlayers() {
@@ -20,53 +20,36 @@ describe('create game', function () {
 
     beforeEach(function () {
         browser.get(createGameUrl);
+        browser.sleep(500);
+        browser.driver.switchTo().alert().accept();
     });
 
-    beforeEach(function () {
-        player1Input = $$('input').get(0);
-        player2Input = $$('input').get(1);
+    it('should redirect to game page', function () {
+        expect(browser.getCurrentUrl()).toMatch(/\/game\/[0-9a-zA-Z]+#\/?$/);
     });
 
     it('should have exacly 1 create game form', function () {
-        expect($$('#crateGameForm').count()).toBe(1);
+        expect($$('#createGameForm').count()).toBe(1);
     });
 
-    it('should have exactly 8 input for player names', function () {
+    it('should present only 2 field by default', function () {
         var playerInput = element.all(by.repeater('player in players'));
-        expect(playerInput.count()).toBe(numberPlayers);
-    });
-
-    it('should not create game without players', function () {
-        submitFormNotEngouthPlayers();
+        expect(playerInput.count()).toBe(initialNumberOfPlayers);
     });
 
     it('should not create game with only 1 player', function () {
-        player1Input.sendKeys('player1').then(submitFormNotEngouthPlayers);
+        submitFormNotEngouthPlayers();
     });
 
     it('should create game with 2 players', function () {
-        enterPlayer1()
-                .then(enterPlayer2)
-                .then(submitForm)
-                .then(check);
+        var browserPlayer2 = browser.forkNewDriverInstance(true, true);
+        browserPlayer2.driver.switchTo().alert().accept();
+        browser.sleep(500);
 
-        function enterPlayer1() {
-            return player1Input.sendKeys('player1');
-        }
+        createGameForm.click();
 
-        function enterPlayer2() {
-            return player2Input.sendKeys('player2');
-        }
-
-        function submitForm() {
-            return createGameForm.click();
-        }
-
-        function check() {
-            // We must wait for the browser to go to the right page.
-            browser.sleep(500);
-            expect(browser.getCurrentUrl()).toMatch(/\/game\/[0-9]+$/);
-        }
+        browser.sleep(500);
+        expect(browser.getCurrentUrl()).toMatch(/\/game\/[0-9a-zA-Z]+#\/?game$/);
     });
 
 });
