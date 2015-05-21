65498457b4c993f5f1c0fb1277805bc92dca78e4
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-21 23:55:56 +0200

    ref(gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,34 @@
 'use strict';
 
-var gulp = require('gulp');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var watch = require('gulp-watch');
-var concatCss = require('gulp-concat-css');
-var minifyCss = require('gulp-minify-css');
-var connect = require('gulp-connect');
-var rename = require('gulp-rename');
-var proxy = require('proxy-middleware');
-var renameRegex = require('gulp-regex-rename');
-var data = require('gulp-data');
-var ini = require('ini');
-var fs = require('fs');
-var nunjucksRender = require('gulp-nunjucks-render');
+
 var del = require('del');
+var fs = require('fs');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var connect = require('gulp-connect');
+var data = require('gulp-data');
+var gulpif = require('gulp-if');
+var minifyCss = require('gulp-minify-css');
+var nunjucksRender = require('gulp-nunjucks-render');
+var renameRegex = require('gulp-regex-rename');
+var rename = require('gulp-rename');
+var showHelp = require('gulp-showhelp');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var ini = require('ini');
+var proxy = require('proxy-middleware');
 
 
 var config = {
     dev: true,
     srcJS: ['src/main/webapp/js/lib/angular.js',
-            'src/main/webapp/js/lib/*.js',
-            'src/main/webapp/js/app.js',
-            'src/main/webapp/js/app/**/*module.js',
-            'src/main/webapp/js/app/**/*.js'
-	   ],
+        'src/main/webapp/js/lib/*.js',
+        'src/main/webapp/js/app.js',
+        'src/main/webapp/js/app/**/*module.js',
+        'src/main/webapp/js/app/**/*.js'
+    ],
     destJS: 'prd/js/',
     outJS: 'game.js',
     watchJS: 'src/main/webapp/js/app/**/*.js',
@@ -40,106 +42,136 @@ var config = {
     scrPartials: 'src/main/webapp/js/app/**/*.html',
     destPartials: 'prd/partials',
     srcImg: 'src/main/webapp/img/**/*',
-    destImg: 'prd/img'
+    destImg: 'prd/img',
+    templates: {}
 };
 
+// We cannot use the default tags since they conflict with angular's
 nunjucksRender.nunjucks.configure({
     tags: {
-	variableStart: '${',
-	variableEnd: '}',
+        variableStart: '${',
+        variableEnd: '}'
     },
     watch: false
 });
 
-gulp.task('set-production', function () {
-    config.dev = false;
+
+gulp.task('default', ['help']);
+
+
+gulp.task('help', function () {
+    showHelp.show(showHelp.taskNames().sort());
 });
 
-gulp.task('prod', ['set-production',
-		   'build-js',
-		   'build-css',
-		   'build-html',
-		   'build-images'
-		  ]);
+
+gulp.task('clean', function (cb) {
+    del('prd', cb);
+}).help = 'clean production files.';
+
+
+gulp.task('cleanall', ['clean'], function (cb) {
+    del('node_modules', cb);
+}).help = 'launch clean and remove all node_modules.';
+
+
+gulp.task('dev', [
+    'load-dev-conf',
+    'build-js',
+    'build-css',
+    'build-html',
+    'build-partials',
+    'build-images'
+]).help = 'build all files for development.';
+
+
+gulp.task('load-dev-conf', function () {
+    config.templates = ini.parse(fs.readFileSync('./config-dev.ini', 'utf-8'));
+});
+
 
 gulp.task('build-js', function () {
     return gulp.src(config.srcJS)
         .pipe(gulpif(config.dev, sourcemaps.init()))
-	.pipe(concat(config.outJS))
+        .pipe(concat(config.outJS))
         .pipe(uglify())
         .pipe(gulpif(config.dev, sourcemaps.write()))
         .pipe(gulp.dest(config.destJS));
 });
 
+
 gulp.task('build-css', function () {
     return gulp.src(config.srcCSS)
-	.pipe(gulpif(config.dev, sourcemaps.init()))
-	.pipe(concatCss(config.outCSS))
-	.pipe(minifyCss())
-	.pipe(gulpif(config.dev, sourcemaps.write()))
+        .pipe(gulpif(config.dev, sourcemaps.init()))
+        .pipe(concatCss(config.outCSS))
+        .pipe(minifyCss())
+        .pipe(gulpif(config.dev, sourcemaps.write()))
         .pipe(gulp.dest(config.destCSS));
 });
 
+
 gulp.task('build-html', function () {
     return gulp.src(config.srcHtml)
-	.pipe(data(function() {
-            var nunjucksConfig = {};
-            if (config.dev) {
-	        nunjucksConfig = ini.parse(fs.readFileSync('./config-dev.ini', 'utf-8'));
-            } else {
-                nunjucksConfig = ini.parse(fs.readFileSync('./config-prod.ini', 'utf-8'));
-            }
-	    return nunjucksConfig;
-	}))
-	.pipe(nunjucksRender())
-	.pipe(rename('game/index.html'))
-	.pipe(gulp.dest(config.destHtml));
+        .pipe(data(function () {
+            var nunjucksConfig = config.templates;
+            return nunjucksConfig;
+        }))
+        .pipe(nunjucksRender())
+        .pipe(rename('game/index.html'))
+        .pipe(gulp.dest(config.destHtml));
 });
 
-gulp.task('build-images', function () {
-    return gulp.src(config.srcImg)
-	.pipe(gulp.dest(config.destImg));
-});
 
 gulp.task('build-partials', function () {
     return gulp.src(config.scrPartials)
-	.pipe(renameRegex(/\/partials/, ''))
-	.pipe(gulp.dest(config.destPartials));
+        .pipe(renameRegex(/\/partials/, ''))
+        .pipe(gulp.dest(config.destPartials));
 });
 
-gulp.task('watch', ['build-js',
-		    'build-css',
-		    'build-html',
-		    'build-partials',
-		    'build-images',
-		    'connect'],
-	  function () {
-    watch(config.watchJS, function() {
+
+gulp.task('build-images', function () {
+    return gulp.src(config.srcImg)
+        .pipe(gulp.dest(config.destImg));
+});
+
+
+gulp.task('watch', ['dev', 'serve'], function () {
+    watch(config.watchJS, function () {
         gulp.start('build-js');
     });
-    watch(config.watchCSS, function() {
+    watch(config.watchCSS, function () {
         gulp.start('build-css');
     });
     watch(config.watchHtml, function () {
-	gulp.start('build-html');
+        gulp.start('build-html');
     });
-});
+}).help = 'watch files for modification and rebuild them.';
 
-gulp.task('connect', function() {
+
+gulp.task('serve', function () {
     connect.server({
-	root: 'prd',
-	port: 8282,
-	middleware: function(connect, o) {
-            return [ (function() {
-		var url = require('url');
-		var options = url.parse('http://localhost:8080/api');
-		options.route = '/api';
-		return proxy(options);
-            })() ];
-	}
+        root: 'prd',
+        port: 8282,
+        middleware: function () {
+            return [(function () {
+                    var url = require('url');
+                    var options = url.parse('http://localhost:8080/api');
+                    options.route = '/api';
+                    return proxy(options);
+                })()];
+        }
     });
-});
+}).help = 'start a small server to ease devolopment of the frontend';
 
-gulp.task('clean', function (cb) {
-    del('prd', cb);
-})
+
+gulp.task('prod', ['load-prod-conf',
+    'build-js',
+    'build-css',
+    'build-html',
+    'build-images'
+]).help = 'build all files for production.';
+
+
+gulp.task('load-prod-conf', function () {
+    config.dev = false;
+    config.templates = ini.parse(fs.readFileSync('./config-prod.ini', 'utf-8'));
+});
