cd4613e7eb136cfa2d7e771f7136af310de6bc50
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-24 17:45:34 +0200

    chore(deploy.sh): can update running container

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,17 @@
 
 if [[ $(hostname) == giskard ]]; then
     cd ~/aot/docker
+
+    # Stop the running container if it exists and delete it
+    running_container_id=$(docker ps -a | grep aot | awk '{FS=" "; print $1}')
+    if [[ -n "${running_container_id}" ]]; then
+        docker stop "${running_container_id}"
+        docker rm "${running_container_id}"
+        image_id=$(docker  images | grep aot | awk '{FS=" "; print $3}')
+        docker rmi "${image_id}"
+    fi
+
+    # Build and run the new image
     docker build -t aot .
     docker run --name aot -d -p 8280:8080 aot
 else
