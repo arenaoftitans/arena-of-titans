66753ee3b5a3efd1aca849e386917863442abb45
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-04-03 17:16:38 +0200

    feat(create/player-infos): make carousel infinite

--- app/game/widgets/popups/player-infos.js
+++ app/game/widgets/popups/player-infos.js
@@ -7,63 +7,126 @@ export class AotPlayerInfosCustomElement {
     @bindable done = null;
 
     constructor() {
+        this.currentHeroIndex = 0;
+        this.direction = null;
+
         this.heroes = [];
         for (let hero of Game.heroes) {
             this.heroes.push({
                 name: hero,
-                class: 'hidden',
+                next: null,
+                previous: this.heroes.length > 0 ? this.heroes[this.heroes.length - 1] : null,
             });
         }
-        this.heroStyles = [];
-        this.currentHeroIndex = 0;
+        this.heroes[0].previous = this.heroes[this.heroes.length - 1];
+        this.heroes.forEach((hero, index) => {
+            if (index < this.heroes.length - 1) {
+                let nextIndex = index + 1;
+                hero.next = this.heroes[nextIndex];
+            } else {
+                hero.next = this.heroes[0];
+            }
+        });
     }
 
     bind() {
         if (this.data !== null && this.data.hero) {
             this.currentHeroIndex = Game.heroes.indexOf(this.data.hero);
+        } else {
+            this.data.hero = this.currentHero.name;
         }
 
-        this.updateHeroesDisplay();
+        let defered = {};
+        defered.promise = new Promise((resolve) => {
+            defered.resolve = resolve;
+        });
+
+        (function waitForCarousel() {
+            let heroesCarousel = document.getElementById('heroes-carousel');
+            if (heroesCarousel !== null) {
+                defered.resolve(heroesCarousel);
+            } else {
+                setTimeout(waitForCarousel, 500);
+            }
+        })();
+
+        defered.promise.then(heroesCarousel => {
+            this.heroesCarousel = heroesCarousel;
+            this.setHeroImage(
+                this.heroesCarousel.children[0],
+                this.currentHero.previous.previous.name);
+            this.setHeroImage(this.heroesCarousel.children[1], this.currentHero.previous.name);
+            this.setHeroImage(this.heroesCarousel.children[2], this.currentHero.name);
+            this.setHeroImage(this.heroesCarousel.children[3], this.currentHero.next.name);
+            this.setHeroImage(this.heroesCarousel.children[4], this.currentHero.next.next.name);
+        });
+    }
+
+    setHeroImage(element, name) {
+        element.src = `/assets/game/heroes/${name}.png`;
+        element.alt = name;
     }
 
     updateHeroesDisplay() {
         if (this.data !== null) {
-            this.data.hero = this.heroes[this.currentHeroIndex].name;
+            this.data.hero = this.currentHero.name;
         }
 
-        for (let i = 0; i < this.heroes.length; i++) {
-            if (i === this.currentHeroIndex) {
-                this.heroes[i].class = 'main-pos';
-            } else if (i === this.currentHeroIndex - 1) {
-                this.heroes[i].class = 'left-pos';
-            } else if (i === this.currentHeroIndex + 1) {
-                this.heroes[i].class = 'right-pos';
-            } else {
-                this.heroes[i].class = 'hidden';
-            }
+        if (this.direction === 'left') {
+            this.heroesCarousel.children[0].className = 'hero-img left-pos';
+            this.heroesCarousel.children[1].className = 'hero-img main-pos';
+            this.heroesCarousel.children[2].className = 'hero-img right-pos';
+            this.heroesCarousel.children[3].className = 'hero-img hidden';
+            let newImage = document.createElement('img');
+            this.setHeroImage(newImage, this.currentHero.previous.previous.name);
+            newImage.className = 'hero-img hidden';
+            this.heroesCarousel.removeChild(this.heroesCarousel.children[4]);
+            this.heroesCarousel.insertBefore(newImage, this.heroesCarousel.children[0]);
+        } else if (this.direction === 'right') {
+            this.heroesCarousel.children[1].className = 'hero-img hidden';
+            this.heroesCarousel.children[2].className = 'hero-img left-pos';
+            this.heroesCarousel.children[3].className = 'hero-img main-pos';
+            this.heroesCarousel.children[4].className = 'hero-img right-pos';
+            let newImage = document.createElement('img');
+            this.setHeroImage(newImage, this.currentHero.next.next.name);
+            newImage.className = 'hero-img hidden';
+            this.heroesCarousel.appendChild(newImage);
+            this.heroesCarousel.removeChild(this.heroesCarousel.children[0]);
         }
     }
 
-    nextHero() {
+    viewNextHero() {
+        this.direction = 'right';
         if (this.hasNextHero) {
             this.currentHeroIndex++;
-            this.updateHeroesDisplay();
+        } else {
+            this.currentHeroIndex = 0;
         }
+
+        this.updateHeroesDisplay();
     }
 
     get hasNextHero() {
         return this.currentHeroIndex < this.heroes.length - 1;
     }
 
-    previousHero() {
+    viewPreviousHero() {
+        this.direction = 'left';
         if (this.hasPreviousHero) {
             this.currentHeroIndex--;
-            this.updateHeroesDisplay();
+        } else {
+            this.currentHeroIndex = this.heroes.length - 1;
         }
+
+        this.updateHeroesDisplay();
     }
 
     get hasPreviousHero() {
         return this.currentHeroIndex > 0;
+    }
+
+    get currentHero() {
+        return this.heroes[this.currentHeroIndex];
     }
 
     save() {
