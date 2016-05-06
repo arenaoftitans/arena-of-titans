5131b812da460e370178bd6a06c805ab4758c106
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-05-06 17:49:28 +0200

    feat(popup): change background for victory popup

--- app/game/widgets/popups/popup.js
+++ app/game/widgets/popups/popup.js
@@ -5,4 +5,17 @@ export class AotPopupCustomElement {
     @bindable data = null;
     @bindable type = null;
     @bindable done = null;
+
+    background = '';
+
+    bind() {
+        switch (this.type) {
+            case 'game-over':
+                this.background = 'game-over';
+                break;
+            default:
+                this.background = 'default';
+                break;
+        }
+    }
 }
