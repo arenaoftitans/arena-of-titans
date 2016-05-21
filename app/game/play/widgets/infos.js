528ac468376bf6e41e5682bc0276307982ee6159
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-05-21 15:53:29 +0200

    fix(infos): improve position and appear time
    - Correct formulas for positions
    - Improve code for component
    - Make element visible at all time to get correct height and width from
    bounding box. Hide it by putting it outside the screen.

--- app/game/play/widgets/infos.js
+++ app/game/play/widgets/infos.js
@@ -1,61 +1,96 @@
 import { bindable, inject, ObserverLocator } from 'aurelia-framework';
+import { Wait } from '../../services/utils';
 
 
 @inject(ObserverLocator)
 export class AotInfosCustomElement {
     @bindable type = null;
     @bindable infos = null;
-    display = false;
-    _timeout;
+    element = null;
+    height
+    timeout;
+    waitForElement;
+    width;
 
     constructor(observerLocator) {
-        observerLocator
-            .getObserver(this, 'infos')
-            .subscribe(() => {
-                this.display = false;
-                if (this._timeout !== undefined) {
-                    clearTimeout(this._timeout);
-                }
+        observerLocator.getObserver(this, 'infos').subscribe(() => {
+            if (this.element === null) {
+                this.init();
+            } else {
+                this.hide();
+            }
 
-                if (!this.infos.event) {
-                    return;
-                } else if (this.infos.visible) {
-                    this._timeout = setTimeout(() => (this.display = true, 2500));
-                }
+            if (this.timeout !== undefined) {
+                clearTimeout(this.timeout);
+            }
 
+            if (!this.infos.event) {
+                return;
+            } else if (this.infos.visible) {
+                let target = this.infos.event.target;
+                this.timeout = setTimeout(() => this.show(target), 2500);
+            }
+        });
+    }
 
-                if (this.type === 'trumps') {
-                    let trumpsContainer = document.getElementById('player-trumps');
-                    let infosElement = document.getElementById('trumps-element-infos');
-                    let target = this.infos.event.target;
+    init() {
+        let infosId = this.getInfosId();
+        (new Wait()).forId(infosId).then(element => {
+            this.element = element;
+            let boundingBox = element.getBoundingClientRect();
+            this.width = boundingBox.width;
+            this.height = boundingBox.height;
+            this.hide();
+        });
+    }
 
-                    let infosHeight = 150;
-                    let top = 0;
-                    let element = target;
-                    do {
-                        top += element.offsetTop  || 0;
-                        element = element.offsetParent;
-                    } while (element);
+    getInfosId() {
+        switch (this.type) {
+            case 'cards':
+                return 'cards-element-infos';
+            case 'trumps':
+                return 'trumps-element-infos';
+            default:
+                throw new Error('Unsuported typein aot-info: ' + this.type);
+        }
+    }
 
-                    infosElement.style.top = top - infosHeight + 'px';
-                    infosElement.style.left = trumpsContainer.getBoundingClientRect().width + 'px';
-                } else if (this.type === 'cards') {
-                    let cardsContainer = document.getElementById('cards-container');
-                    let infosElement = document.getElementById('cards-element-infos');
-                    let target = this.infos.event.target;
+    show(target) {
+        if (this.type === 'trumps') {
+            let trumpsContainer = document.getElementById('player-trumps');
 
-                    let halfCardWidth = 69 / 2;
-                    let halfInfosWidth = 150 / 2;
+            let top = 0;
+            let element = target;
+            do {
+                top += element.offsetTop || 0;
+                element = element.offsetParent;
+            } while (element);
 
-                    infosElement.style.bottom = cardsContainer.getBoundingClientRect().bottom -
-                        cardsContainer.getBoundingClientRect().top +
-                        40 +
-                        'px';
-                    infosElement.style.left = target.getBoundingClientRect().left -
-                        halfInfosWidth +
-                        halfCardWidth +
-                        'px';
-                }
-            });
+            this.element.style.top = top -
+                this.height -
+                target.getBoundingClientRect().height +
+                'px';
+            this.element.style.left = trumpsContainer.getBoundingClientRect().width + 'px';
+        } else if (this.type === 'cards') {
+            let targetBoundingClientRect = target.getBoundingClientRect();
+
+            let halfCardWidth = targetBoundingClientRect.width / 2;
+            let halfInfosWidth = this.width / 2;
+
+            this.element.style.top = targetBoundingClientRect.top -
+                this.height -
+                10 +
+                'px';
+            this.element.style.left = target.getBoundingClientRect().left -
+                halfInfosWidth +
+                halfCardWidth +
+                'px';
+        }
+    }
+
+    hide() {
+        if (this.element) {
+            this.element.style.top = '-500px';
+        }
     }
 }
