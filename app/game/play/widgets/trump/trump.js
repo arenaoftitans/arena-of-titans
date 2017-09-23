dc89bffc7cbb869faa4c221078d9ffcabbba5067
Julien Enselme <jenselme@jujens.eu>
2017-09-23 22:21:38 +0200

    feat(infos): correctly translate for powers

--- app/game/play/widgets/trump/trump.js
+++ app/game/play/widgets/trump/trump.js
@@ -97,7 +97,7 @@ export class AotTrumpCustomElement {
                 break;
             case 'power':
                 this.svgClass = 'power-trump';
-                this.infosType = 'trumps';
+                this.infosType = 'power';
                 break;
             default:
                 this.svgClass = undefined;
@@ -134,11 +134,22 @@ export class AotTrumpCustomElement {
     }
 
     getTranslatedTrumpDescription() {
-        return this._i18n.tr(`trumps.${this.normalizeTrumpName()}_description`);
+        let ns = this._getTranslationNamespace();
+        return this._i18n.tr(`${ns}.${this.normalizeTrumpName()}_description`);
+    }
+
+    _getTranslationNamespace() {
+        switch (this.infosType) {
+            case 'power':
+                return 'powers';
+            default:
+                return 'trumps';
+        }
     }
 
     getTranslatedTrumpTitle() {
-        return this._i18n.tr(`trumps.${this.normalizeTrumpName()}`);
+        let ns = this._getTranslationNamespace();
+        return this._i18n.tr(`${ns}.${this.normalizeTrumpName()}`);
     }
 
     hideInfos() {
