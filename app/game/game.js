73a53882786b87c8392da6ef2a44221a8ea951b0
Ismaël NABTI <ismael.nabti@centrale-marseille.fr>
2017-11-20 21:27:54 +0100

    feat(game): trigger an animation when a trump is played

--- app/game/game.js
+++ app/game/game.js
@@ -122,6 +122,37 @@ export class Game {
             this._tutorialInProgress = false;
         });
 
+        // Trump animation
+        this._eas.subscribe('aot:api:play_trump', message => {
+            let popupData = {
+                translate: {
+                    messages: {},
+                },
+            };
+
+            let hero1 = this._api.game.players.heroes[this._api.game.next_player];
+            popupData.heroImg = AssetSource.forHero(hero1);
+            popupData.translate.messages.playerName =
+                this._api.game.players.names[this._currentPlayerIndex];
+
+            let hero2 = this._api.game.players.heroes[message.last_action.target_index];
+            popupData.targetedHeroImg = AssetSource.forHero(hero2);
+            popupData.translate.messages.targetName =
+                this._api.game.players.names[message.last_action.target_index];
+
+            let trump1 = message.last_action.trump;
+            popupData.trumpImg = AssetSource.forTrump(trump1);
+            popupData.translate.messages.trumpName = message.last_action.trump.title;
+
+            let options = {
+                timeout: PLAYER_TRANSITION_POPUP_DISPLAY_TIME,
+            };
+
+            this._popup.display('trump-animation', popupData, options).then(() => {
+                this._eas.publish('aot:game:trump-animation:done', message.trump);
+            });
+        });
+
         this._eas.subscribeMultiple(['aot:api:create_game', 'aot:api:play'], message => {
             if (!this.canDisplayTransitionPopup(message)) {
                 // We update the current player index nonetheless. This way, after viewing or
