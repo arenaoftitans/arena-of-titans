a2240341fa2f4f1d58f7a4a1a693ff8104f08a4f
Julien Enselme <jenselme@jujens.eu>
2017-11-26 15:11:42 +0100

    chore(aurelia): read template variables once

--- aurelia_project/tasks/render-templates.js
+++ aurelia_project/tasks/render-templates.js
@@ -5,8 +5,10 @@ import project from '../aurelia.json';
 import {getTemplatesVariables} from './utils';
 
 export default function renderTemplates() {
+    const variables = getTemplatesVariables();
+
     return gulp.src(project.templates.source)
-        .pipe(data(() => getTemplatesVariables()))
+        .pipe(data(() => variables))
         .pipe(nunjucks.compile())
         .pipe(gulp.dest('.'));
 }
