8b4a93129778ae305eb9a30a8e7fca98a121d8ff
Julien Enselme <jenselme@jujens.eu>
2017-11-26 15:15:28 +0100

    chore(aurelia): can read rollbar configuration file
    This is required to build the rollbar template.

--- aurelia_project/tasks/utils.js
+++ aurelia_project/tasks/utils.js
@@ -6,9 +6,30 @@ export function getVersion() {
 }
 
 
+export function getRollbar() {
+    const configPath = CLIOptions.getFlagValue('rollbar');
+    const env = CLIOptions.getEnvironment();
+
+    if (!configPath) {
+        console.warn('Rollbar configuration was not supplied.');
+        return {};
+    }
+
+    const rollbar = require(configPath);
+    const envConfiguration = rollbar[env];
+    if (!envConfiguration) {
+        console.warn(`Rollbar is not available for ${env}`);
+        return {};
+    }
+
+    return envConfiguration.front;
+}
+
+
 export function getTemplatesVariables() {
     return {
         env: CLIOptions.getEnvironment(),
+        rollbar: getRollbar(),
         version: getVersion(),
     };
 }
