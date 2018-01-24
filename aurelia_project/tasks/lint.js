874753ddb5ae7ff77d7d7abfa221ff01466d5f98
Julien Enselme <jenselme@jujens.eu>
2018-01-24 22:03:21 +0100

    feat(aurelia): replace custom template lint script by a task

--- aurelia_project/tasks/lint.js
+++ aurelia_project/tasks/lint.js
@@ -4,8 +4,29 @@ import aureliaTemplateLint from 'gulp-aurelia-template-lint';
 let foundLintErrors = false;
 
 const main = gulp.series(
+    templateLint,
     failOnErrors
 );
+
+function templateLint() {
+    function mustIgnoreError(error, file) {
+        if (file === '/app/game/play/widgets/trump/trump.html' &&
+                error.message.startsWith('duplicated id:')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    return gulp.src('./app/**/*.html')
+        .pipe(aureliaTemplateLint(null, (error, file) => {
+            if (!mustIgnoreError(error, file)) {
+                foundLintErrors = true;
+                console.log(`[Template lint] ${error.message} [file: ${file} ln: ${error.line} col: ${error.column}]`);
+                if (error.detail) console.log(`  * ${error.detail}`);
+            }
+        }));
+}
 
 function failOnErrors() {
     if (foundLintErrors) {
