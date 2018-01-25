dfd5a662576491d8ffa92b93781c5a75ae6db4cc
Julien Enselme <jenselme@jujens.eu>
2018-01-25 20:48:58 +0100

    feat(utils): add utility function for update-external

--- aurelia_project/tasks/utils.js
+++ aurelia_project/tasks/utils.js
@@ -1,8 +1,37 @@
 import {CLIOptions} from 'aurelia-cli';
 
 
+export function buildObjectWithKeys(keys) {
+    return keys
+        .map(key => ({ [key]: {} }))
+        .reduce((acc, current) => Object.assign(acc, current), {});
+}
+
+
+export function dumpAsExportedData(data) {
+    return 'export default ' + JSON.stringify(data, null, 4) + ';\n';
+}
+
+
+export function flattenArray(array) {
+    return array.reduce((acc, current) => acc.concat(current), []);
+}
+
+
 export function getVersion() {
     return CLIOptions.getFlagValue('version') || 'latest';
+}
+
+
+function getObjectAtPath(obj, path) {
+    for (let pathElement of path) {
+        if (!(pathElement in obj)) {
+            obj[pathElement] = {};
+        }
+        obj = obj[pathElement];
+    }
+
+    return obj;
 }
 
 
@@ -32,4 +61,12 @@ export function getTemplatesVariables() {
         rollbar: getRollbar(),
         version: getVersion(),
     };
+}
+
+
+export function insertInto(obj, path, value) {
+    const insertPath = path.split('.');
+    const insertKey = insertPath.pop();
+    obj = getObjectAtPath(obj, insertPath);
+    obj[insertKey] = value;
 }
