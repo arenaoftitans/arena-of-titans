0d3d199cc202a0b2d8f022c0dcea521e68b0e70d
Julien Enselme <jenselme@jujens.eu>
2018-02-13 19:32:46 +0100

    feat(aurelia): use path with hash in built CSS files

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -1,17 +1,21 @@
+import fs from 'fs';
 import gulp from 'gulp';
+import revReplace from 'gulp-rev-replace';
 import sourcemaps from 'gulp-sourcemaps';
+import transform from 'gulp-transform';
 import sass from 'gulp-sass';
-import sassVariables from 'gulp-sass-variables';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
-import {getVersion} from './utils';
 
 export default function processCSS() {
+  const manifestPath = `${project.assets.manifest.output}/${project.assets.manifest.name}`;
+  const manifest = gulp.src(manifestPath)
+    .pipe(transform('utf-8', content => content.replace('export default ', '')))
+    .pipe(transform('utf-8', content => content.replace(';', '')));
+
   return gulp.src(project.cssProcessor.source)
     .pipe(sourcemaps.init())
-    .pipe(sassVariables({
-      $version: getVersion(),
-    }))
     .pipe(sass().on('error', sass.logError))
+    .pipe(revReplace({ manifest: manifest }))
     .pipe(build.bundle());
 };
