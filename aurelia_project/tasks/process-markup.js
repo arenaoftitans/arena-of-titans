43a85458dae8b23588065c901ca34c4420461158
Julien Enselme <jenselme@jujens.eu>
2018-02-14 13:53:16 +0100

    feat(aurelia): make sure hash versions of assets are loaded by html

--- aurelia_project/tasks/process-markup.js
+++ aurelia_project/tasks/process-markup.js
@@ -3,8 +3,10 @@ import htmlmin from 'gulp-htmlmin';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
 import changedInPlace from 'gulp-changed-in-place';
+import revReplace from 'gulp-rev-replace';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
+import {getManifest} from './utils';
 
 export default function processMarkup() {
   return gulp.src(project.markupProcessor.source)
@@ -22,5 +24,6 @@ export default function processMarkup() {
         removeStyleLinkTypeAttributes: true,
         ignoreCustomFragments: [/\${.*?}/g] // ignore interpolation expressions
     }))
+    .pipe(revReplace({ manifest: getManifest() }))
     .pipe(build.bundle());
 }
