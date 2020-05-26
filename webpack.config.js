ca2634dc10bb176ee1854f2a86d3ca7368d687a2
Julien Enselme <jenselme@jujens.eu>
2020-05-26 21:45:07 +0200

    feat(index): move all code from old index.html template to webpack template

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const when = (condition, config, negativeConfig) =>
     condition ? ensureArray(config) : ensureArray(negativeConfig);
 
 // primary config:
-const title = "Aurelia Navigation Skeleton";
+const title = "Last Run";
 const outDir = path.resolve(__dirname, project.platform.output);
 const assetsOutDir = path.resolve(outDir, "assets");
 const srcDir = path.resolve(__dirname, "app");
