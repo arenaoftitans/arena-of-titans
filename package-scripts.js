76223e1eb6448f03f278fe21331140577af1bb0a
Julien Enselme <jenselme@jujens.eu>
2020-05-28 21:24:02 +0200

    chore(scripts): update how we use scripts
    - Simplify build process
    - Allow arguments to be passed to subscripts
    - Let nps orchestrate the thing

--- package-scripts.js
+++ package-scripts.js
@@ -4,17 +4,19 @@ module.exports = {
     scripts: {
         default: "nps",
         clean: rimraf("dist"),
-        build: {
-            sprites:
-                "glue -s assets/game/cards/movement -o style/sprites --img assets/game/sprites/",
-        },
         update: {
             defaultnames: "au update-external --kind default-names",
+            config: "au build-config",
             translations: series(
                 'i18next-scanner "app/**/*.{js,html}" > /dev/null 2>&1',
                 "au update-external --kind translations",
                 "prettier app/locale/**/*.js --write",
             ),
+        },
+        webpack: {
+            analyse: "webpack --env.production --env.environment=production --analyze",
+            devserver: "webpack-dev-server --env.environment=dev --extractCss",
+            build: "webpack --extractCss",
         },
         lint: series("au lint", 'stylelint "app/**/*.scss"', 'eslint "app/**/*.js" "test/**/*.js"'),
     },
